fdd904b159f5004ff20d71bff2e317c8235e8e20
MrGarsan <estebangabrielgarciasanchez@gmail.com>
2021-10-30 14:31:39 -0300

    header basic config

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,31 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import { AiOutlineMenu } from "react-icons/ai";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <AiOutlineMenu />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            Backoffice
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Header;
