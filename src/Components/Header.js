575945decc3d21d08dd97d162aa93a6d59bc45ba
MrGarsan <estebangabrielgarciasanchez@gmail.com>
2021-11-12 17:29:47 -0300

    fix button header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,14 +11,6 @@ const Header = () => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <AiOutlineMenu />
-          </IconButton>
           <Typography variant="h6" color="inherit" component="div">
             Backoffice
           </Typography>
