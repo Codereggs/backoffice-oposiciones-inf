54a89e6c7f392e36592da02cc6655f770535e896
MrGarsan <estebangabrielgarciasanchez@gmail.com>
2021-11-14 09:55:27 -0300

    fix link warning error

--- src/Components/Details/CardsDashboard.js
+++ src/Components/Details/CardsDashboard.js
@@ -5,7 +5,7 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function CardsDashboard({ titulo, imagen, link, action, desc }) {
   const card = (
